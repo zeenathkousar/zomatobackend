e0cde6212b3326938f38913fdf3bc5ca4e8231a3
zeenathkousar <95568153+zeenathkousar@users.noreply.github.com>
2024-01-06 18:50:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/',(req,res)=>{
     res.send('hello')
 })
 app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin',"https://frontendzomatowebsite.onrender.com/");
+    res.setHeader('Access-Control-Allow-Origin',"*");
     res.header(
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept'
